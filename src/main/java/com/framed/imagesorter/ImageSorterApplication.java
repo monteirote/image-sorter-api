897ab7151768fc99fb703c6f02209b4d1ed5bbb8
Vinicius <vinicius@iconax.com.br>
2023-10-16 10:10:58 -0300

    adicionando projeto spring

---
+++ src/main/java/com/framed/imagesorter/ImageSorterApplication.java
@@ -0,0 +1,13 @@
+package com.framed.imagesorter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageSorterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageSorterApplication.class, args);
+	}
+
+}
