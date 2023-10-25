e1924d649d6dfd660203c54d86a9d0d91d974db9
Vinicius <vinicius@iconax.com.br>
2023-10-25 11:04:37 -0300

    migrando o projeto para Gradle

--- src/main/java/com/framed/imagesorter/controller/exception/ImageExtractingException.java
+++
@@ -1,9 +0,0 @@
-package com.framed.imagesorter.controller.exception;
-
-import com.drew.imaging.ImageProcessingException;
-
-public class ImageExtractingException extends RuntimeException {
-    public ImageExtractingException(String message, ImageProcessingException error) {
-        super(message, error);
-    }
-}
--- src/main/java/com/framed/imagesorter/controller/exception/ImageIOException.java
+++
@@ -1,7 +0,0 @@
-package com.framed.imagesorter.controller.exception;
-
-public class ImageIOException extends RuntimeException {
-    public ImageIOException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
